a9de526996bf988c321aad4ac830b8d2e457f804
Bayron <b.munooz15@ufromail.cl>
2020-09-21 21:30:47 -0400

    Atributos en clase ClienteTCP

--- src/main/java/ClienteTCP/ClienteTCP.java
+++ src/main/java/ClienteTCP/ClienteTCP.java
@@ -1,4 +1,46 @@
 package ClienteTCP;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
 public class ClienteTCP {
+    //Atributos
+    /**
+     *Escribir Tipo de datos primitivos en un flujo de entrada
+     */
+    private DataInputStream input;
+
+    /**
+     *Proporciona almacenamiento en búfer de datos de entrada
+     */
+    private BufferedInputStream bis;
+
+    /**
+     *Proporciona almacenamiento en búfer de datos de salida
+     */
+    private BufferedOutputStream bos;
+
+    /**
+     * Entero
+     */
+    private int in;
+
+    /**
+     *Envuelve un Array de valores de tipo primitivo byte en un objeto
+     */
+    private byte[] byteArray;
+
+    /**
+     * Punto de comunicacion entre dos maquinas
+     */
+    private Socket enchufe;
+
+    /**
+     * Escribir Tipo de datos primitivos en un flujo de salida
+     */
+    private DataOutputStream dos;
 }
+
--- src/main/java/ServidorTCP/ServidorTCP.java
+++ src/main/java/ServidorTCP/ServidorTCP.java
@@ -1,4 +1,50 @@
 package ServidorTCP;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServidorTCP {
+    //Atributos
+    /**
+     *ServerSocket espera a que lleguen solicitudes a travez de la red
+     */
+    private ServerSocket servidor;
+
+    /**
+     *Punto de comunicacion entre dos maquinas
+     */
+    private Socket enchufe;
+
+    /**
+     *Escribir Tipo de datos primitivos en un flujo de salida
+     */
+    private DataOutputStream salida;
+
+    /**
+     *Proporciona almacenamiento en búfer de datos de entrada
+     */
+    private BufferedInputStream bis;
+
+    /**
+     *Proporciona almacenamiento en búfer de datos de salida
+     */
+    private BufferedOutputStream bos;
+
+    /**
+     *Envuelve un valor de tipo primitivo byte en un objeto
+     */
+    private byte[] datosRecibidos;
+
+    /**
+     * Entero
+     */
+    private int in;
+
+    /**
+     * String
+     */
+    private String file;
 }
