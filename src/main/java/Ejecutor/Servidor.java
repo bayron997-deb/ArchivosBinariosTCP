306ed4312f8bcc5e8effb46bdd4e553bc8a8b66f
Bayron <b.munooz15@ufromail.cl>
2020-09-21 21:59:26 -0400

    Metodos main de package ejecutor

--- src/main/java/Ejecutor/Servidor.java
+++ src/main/java/Ejecutor/Servidor.java
@@ -1,4 +1,20 @@
 package Ejecutor;
 
+import ServidorTCP.ServidorTCP;
+
 public class Servidor {
+    //Atributo
+    /**
+     * Objeto de la clase ServidorTCP
+     */
+    public static ServidorTCP server = new ServidorTCP();
+    /**
+     * Metodo para ejecutar Server
+     * @param args
+     */
+    public static void main(String[] args) {
+        //mensaje de bienvenida
+        System.out.println("Bienvenido Servidor");
+        server.servidorTCP();
+    }
 }
