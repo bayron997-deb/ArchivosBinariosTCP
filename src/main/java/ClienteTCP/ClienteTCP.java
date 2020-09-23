3d47734293d038d60561137f1afdafd80c98395d
Bayron <b.munooz15@ufromail.cl>
2020-09-23 13:15:11 -0400

    Mejorar comentarios

--- src/main/java/ClienteTCP/ClienteTCP.java
+++ src/main/java/ClienteTCP/ClienteTCP.java
@@ -8,34 +8,28 @@ import java.util.Scanner;
 
 public class ClienteTCP {
     //Atributos
-    /**
-     *Proporciona almacenamiento en búfer de datos de entrada
-     */
+    //lectura y almacenamiento de datos
     private BufferedInputStream bis;
 
-    /**
-     *Proporciona almacenamiento en búfer de datos de salida
-     */
+    //Buffer interno para almacenar datos de salida
     private BufferedOutputStream bos;
 
-    /**
-     * Entero
-     */
+    //Numero entero
     private int in;
 
-    /**
-     *Envuelve un Array de valores de tipo primitivo byte en un objeto
-     */
+    //Envuelve un Array de valores de tipo primitivo byte en un objeto
     private byte[] byteArray;
 
-    /**
-     * Punto de comunicacion entre dos maquinas
-     */
+    //Socket para conectar al servidor
     private Socket enchufe;
 
-    /**
-     * Escribir Tipo de datos primitivos en un flujo de salida
-     */
+    //Puerto de conexion
+    private int port = 65000;
+
+    //Direccion Ip maquina
+    private String address = "localhost";
+
+    //Flujo de datos de salida
     private DataOutputStream dos;
 
     //ruta del fichero que se quiere transferir
@@ -67,26 +61,48 @@ public class ClienteTCP {
             while (segui) {
                 //Mostrar contenido directorio
                 mostrarDirectorio();
-                //Mensaje
+
+                //Mensaje del archivo que se quiere transferir
                 System.out.println("Ingrese nombre del archivo que quiere subir");
                 //Respuesta
                 nombre = teclado.next();
+
+                //ruta del archivo que se va a transferir
                 filename = "F:\\Tareas\\ArchivosCliente\\" + nombre;
-                //enviar un archivo
-                final File localFile = new File(filename);
-                enchufe = new Socket("localhost", 57000);
+                //Crear un localfile de tipo File con la ruta del archivo
+                File localFile = new File(filename);
+
+                //Creamos un socket de transmision en la direccion y puerto especifico
+                enchufe = new Socket(address, port);
+
+                //Leemos el flujo de datos de localfile y los almacenamos en el buffer de entrada
                 bis = new BufferedInputStream(new FileInputStream(localFile));
+                //almacenamos el flujo de salida del socket
                 bos = new BufferedOutputStream(enchufe.getOutputStream());
+
                 //Enviar el nombre del fichero
-                dos = new DataOutputStream(enchufe.getOutputStream());
+
+                //Crea un flujo de salida de datos para escribir datos
+                dos = new DataOutputStream(enchufe.getOutputStream());//flujo de datos hacia el servidor
+                //escribe en UTF el nombre del archivo que se va a transferir
                 dos.writeUTF(localFile.getName());
+
                 //Enviar fichero
+
+                //buffer para leer y escribir bloques de 8kb
                 byteArray = new byte[8192];
-                while ((in = bis.read(byteArray)) != -1) {
+                //loop para escribir en bystes
+                while ((in = bis.read(byteArray)) != -1) { //lee todos los bytes hasta que devuelve -1, marca termino del archivo
+                    //almacena los bytes ecritos en byteArray en el buffer de salida
                     bos.write(byteArray, 0, in);
                 }
+
+                //livera recursos y cierra el flujo de entrada
                 bis.close();
+                //libera recursos y cierra el flujo de salida
                 bos.close();
+
+                //Confirmacion para subir otro archivo
                 System.out.println("Desea subir otro archivo 1: si");
                 seleccion = teclado.nextInt();
                 if (seleccion==1){
@@ -96,6 +112,7 @@ public class ClienteTCP {
                 }
             }
         } catch (Exception e) {
+            //mensaje de error
             System.err.println(e);
         }
     }
